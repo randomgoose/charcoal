9ebce5e5625c876e41ecbc37075f8510cd8e4c5a
randomgoose <c.chen.ed5557@gmail.com>
2020-03-09 00:43:48 +0000

    more

--- src/Components/ThreeText.js
+++ src/Components/ThreeText.js
@@ -4,7 +4,7 @@ import { useLoader, useUpdate, useFrame} from 'react-three-fiber'
 
 const ThreeText = forwardRef(({ children, vAlign = 'center', hAlign = 'center', size = 1, color = '#000000', ...props }, ref) => {
   const font = useLoader(THREE.FontLoader, '/bold.json')
-  const config = useMemo(() => ({ font, size: 1, height: .1 }), [font])
+  const config = useMemo(() => ({ font, size: 1, height: .01 }), [font])
   const mesh = useUpdate(
     self => {
       const size = new THREE.Vector3()
@@ -17,7 +17,7 @@ const ThreeText = forwardRef(({ children, vAlign = 'center', hAlign = 'center', 
   )
 
   return (
-    <group ref={ref} {...props} scale={[0.1 * size, 0.1 * size, 0.1]}>
+    <group ref={ref} {...props} scale={[0.05 * size, 0.05 * size, 0.05]}>
       <mesh ref={mesh} position={props.position} rotation={props.rotation}>
         <textGeometry attach="geometry" args={[children, config]} />
         <meshStandardMaterial attach="material" />
